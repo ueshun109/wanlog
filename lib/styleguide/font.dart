604af8dc8132d2d5548f44db0f510751279f7f5a
ueshun109 <ush109rock@gmail.com>
2022-01-08 18:55:15 +0900

    :recycle: Refactor font management class

--- lib/styleguide/font.dart
+++ lib/styleguide/font.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
 class FontSize {
+  static const double small = 12.0;
   static const double medium = 16.0;
   static const double large = 24.0;
 }
 
 class Font {
-  static TextStyle notoSansRegular(double fontSize) {
+  static TextStyle notoSansRegular(double fontSize, {Color? color}) {
     return TextStyle(
+      color: color,
       fontSize: fontSize,
       fontFamily: 'NotoSansJP-Regular',
       fontWeight: FontWeight.normal,
     );
   }
 
-  static TextStyle notoSansBold(double fontSize) {
+  static TextStyle notoSansBold(double fontSize, {Color? color}) {
     return TextStyle(
+      color: color,
       fontSize: fontSize,
       fontFamily: 'NotoSansJP-Regular',
       fontWeight: FontWeight.bold,
