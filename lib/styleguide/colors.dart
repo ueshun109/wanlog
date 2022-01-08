c5d2fa46e729588291f3dfac896ede4d84d534fd
ueshun109 <ush109rock@gmail.com>
2022-01-08 18:54:36 +0900

    :+1: Add color management class.

---
+++ lib/styleguide/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+extension WanlogColor on Colors {
+  static const Color primary = Color(0xFFD74827);
+  static const Color primary20 = Color(0x33D74827);
+  static const Color primary60 = Color(0x99D74827);
+  static const Color labelSecondary = Color(0x99000000);
+  static const Color backgroundPrimary = Color(0xFFFFFFFF);
+}
