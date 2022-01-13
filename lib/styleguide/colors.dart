bdd4f714d1e7afc095f485c110b6c010a3dd8e8d
ueshun109 <ush109rock@gmail.com>
2022-01-13 20:21:24 +0900

    :+1: Add color

--- lib/styleguide/colors.dart
+++ lib/styleguide/colors.dart
@@ -5,5 +5,6 @@ extension WanlogColor on Colors {
   static const Color primary20 = Color(0x33D74827);
   static const Color primary60 = Color(0x99D74827);
   static const Color labelSecondary = Color(0x99000000);
-  static const Color backgroundPrimary = Color(0xFFFFFFFF);
+  static const Color backgroundPrimary = Color(0xFFF0F0F0);
+  // static const Color backgroundPrimary = Color(0xFFF7F7F7);
 }
